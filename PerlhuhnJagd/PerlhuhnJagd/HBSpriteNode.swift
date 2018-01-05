57cf8c4a53f2d783d305e98b663b2d54da657bb1
hub <hub@imac.local>
2018-01-05 18:17:50 +0100

    HB Sprite Node added

--- PerlhuhnJagd/PerlhuhnJagd/HBSpriteNode.swift
+++ PerlhuhnJagd/PerlhuhnJagd/HBSpriteNode.swift
@@ -12,11 +12,16 @@ class HBSpriteNode: SKSpriteNode {
     
     let ShotCount : Int = Int.randomFromRange(range: Range(10 ... 30))
     
-    required init(coder: NSCoder){
-        super.init(coder: coder)!
+    fileprivate func setTexture() {
         let image =  #imageLiteral(resourceName: "DSC00885")//do your setup here to make a UIImage
         let texture = SKTexture(image: image)
         self.texture = texture
+    }
+    
+    required init(coder: NSCoder){
+        super.init(coder: coder)!
+        
+        setTexture()
     }
 }
 
